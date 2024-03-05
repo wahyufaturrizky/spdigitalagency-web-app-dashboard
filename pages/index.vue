b9c2d7b23ee74a3fc2c317bc554a201dfb2f5084
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-03-06 02:44:34 +0700

    FEAT: avatars group

--- pages/index.vue
+++ pages/index.vue
@@ -54,17 +54,10 @@
           <template v-if="column.key === 'users'">
             <a-avatar-group>
               <a-avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=1" />
-              <a href="https://www.antdv.com">
-                <a-avatar style="background-color: #f56a00">K</a-avatar>
-              </a>
-              <a-tooltip title="Ant User" placement="top">
-                <a-avatar style="background-color: #87d068">
-                  <template #icon><UserOutlined /></template>
-                </a-avatar>
-              </a-tooltip>
-              <a-avatar style="background-color: #1890ff">
-                <template #icon><AntDesignOutlined /></template>
-              </a-avatar>
+              <a-avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=2" />
+              <a-avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=3" />
+              <a-avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=4" />
+              <a-avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=5" />
             </a-avatar-group>
           </template>
           <template v-if="column.key === 'about'">
